59972c5ea8df1281d45905757697bd87edd85730
Paige Niedringhaus <paigen11@gmail.com>
2019-03-19 16:26:31 -0400

    adding more test stuff

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -3,9 +3,16 @@ import { render, cleanup } from 'react-testing-library';
 import Counter from './Counter';
 
 test('<Counter />', () => {
-  const wrapper = render(<Counter />);
+  // renders component (with destructuring to remove wrapper)
+  const { debug, getByTestId, getByText } = render(<Counter />);
 
-  // the easiest way to see exactly what your test is looking at
-  wrapper.debug();
-  expect(wrapper.getByText('0').tagName).toBe('BUTTON');
+  // the easiest way to see exactly what your test is looking at, outputs dom as string
+  debug();
+
+  // asserts text value 0 is found within a button
+  expect(getByText('0').tagName).toBe('BUTTON');
+  // asserts counter button is a button
+  expect(getByTestId('counter-button').tagName).toBe('BUTTON');
+  // asserts counter buttons starts at 0
+  expect(getByTestId('counter-button').textContent).toBe('0');
 });
