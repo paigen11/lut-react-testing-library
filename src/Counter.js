22cce23b2395c7153078bb43e083da0a6543f4a0
Paige Niedringhaus <paigen11@gmail.com>
2019-03-19 16:37:37 -0400

    mo testing;

--- src/Counter.js
+++ src/Counter.js
@@ -4,11 +4,19 @@ export default class Counter extends Component {
   state = {
     count: 0,
   };
+
+  increment = () => {
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
+  };
   render() {
     const { count } = this.state;
     return (
       <div className="hello">
-        <button data-testid="counter-button">{count}</button>
+        <button data-testid="counter-button" onClick={this.increment}>
+          {count}
+        </button>
       </div>
     );
   }
--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Counter from './Counter';
+
+afterEach(cleanup);
 
 test('<Counter />', () => {
   // renders component (with destructuring to remove wrapper)
   const { debug, getByTestId, getByText } = render(<Counter />);
 
   // the easiest way to see exactly what your test is looking at, outputs dom as string
-  debug();
+  // debug();
+  const counterButton = getByTestId('counter-button');
 
   // asserts text value 0 is found within a button
   expect(getByText('0').tagName).toBe('BUTTON');
   // asserts counter button is a button
-  expect(getByTestId('counter-button').tagName).toBe('BUTTON');
+  expect(counterButton.tagName).toBe('BUTTON');
   // asserts counter buttons starts at 0
-  expect(getByTestId('counter-button').textContent).toBe('0');
+  expect(counterButton.textContent).toBe('0');
+
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('1');
+
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('2');
+  // debug(); // super cool, because it shows the dom state after the two clicks
 });
