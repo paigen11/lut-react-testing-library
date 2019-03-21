61efd1afe9148389f1278e8cb01bc88df44f74c3
Paige Niedringhaus <paigen11@gmail.com>
2019-03-21 17:17:05 -0400

    form input validation tests

--- src/MovieForm.js
+++ src/MovieForm.js
@@ -4,21 +4,23 @@ export default class MovieForm extends Component {
   state = {
     text: '',
   };
+
   render() {
+    // eslint-disable-next-line react/prop-types
     const { submitForm } = this.props;
     const { text } = this.state;
 
     return (
       <div>
-        <form
-          data-testid="movie-form"
-          onSubmit={() =>
-            submitForm({
-              text,
-            })
-          }
-        >
-          <input type="text" />
+        <form data-testid="movie-form" onSubmit={() => submitForm({ text })}>
+          <label htmlFor="text">
+            Text
+            <input
+              type="text"
+              id="text"
+              onChange={e => this.setState({ text: e.target.value })}
+            />
+          </label>
           <button type="submit">Submit</button>
         </form>
       </div>
--- src/MovieForm.test.js
+++ src/MovieForm.test.js
@@ -4,15 +4,25 @@ import MovieForm from './MovieForm';
 
 afterEach(cleanup);
 
+// eslint-disable-next-line no-undef
 const onSubmit = jest.fn();
 
 test('<MovieForm />', () => {
-  const { queryByTestId, getByText } = render(
+  const { queryByTestId, getByText, getByLabelText } = render(
     <MovieForm submitForm={onSubmit} />,
   );
   expect(queryByTestId('movie-form')).toBeTruthy();
 
-  fireEvent.click(getByText('Submit'));
+  // getByLabelText('Text').value = 'hello';
+  // fireEvent.change(getByLabelText('Text'));
+  // this does the same as the two lines above in one fell swoop
+  fireEvent.change(getByLabelText('Text'), {
+    target: { value: 'hello' },
+  });
 
+  fireEvent.click(getByText('Submit'));
   expect(onSubmit).toHaveBeenCalledTimes(1);
+  expect(onSubmit).toHaveBeenCalledWith({
+    text: 'hello',
+  });
 });
