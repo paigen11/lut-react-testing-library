7b08c8b55eed9054eaf7a0119ba99948f3efeff5
Paige Niedringhaus <paigen11@gmail.com>
2019-03-23 19:23:19 -0400

    unhappy path tests too

--- src/Movie.test.js
+++ src/Movie.test.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
+// memory router is the fake router needed for react router tests
+import { MemoryRouter } from 'react-router-dom';
 import Movie from './Movie';
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup();
+  // need to clean up the mocks after each test
+  // to prevent console.error from failing the second test
+  console.error.mockClear();
+});
 
 console.error = jest.fn();
 
 test('<Movie />', () => {
   render(<Movie />);
-  expect(console.error).toBeCalled();
+  expect(console.error).toHaveBeenCalled();
+});
+
+const movie = {
+  id: 'hi',
+  title: 'Level Up Big Day Out',
+  poster_path: 'avfklerhfr.jpg',
+};
+
+test('<Movie /> with movie', () => {
+  render(
+    <MemoryRouter>
+      <Movie movie={movie} />
+    </MemoryRouter>,
+  );
+  expect(console.error).not.toHaveBeenCalled();
 });
