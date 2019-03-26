112867927bfd7f4e2bb3868ee7e341944c00ca23
Paige Niedringhaus <paigen11@gmail.com>
2019-03-26 16:12:41 -0400

    testing more data

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 import Overdrive from 'react-overdrive';
 import { Poster } from './Movie';
 
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
-const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
+export const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+export const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MovieDetail extends Component {
   state = {
@@ -30,20 +30,25 @@ class MovieDetail extends Component {
 
   render() {
     const { movie } = this.state;
+    if (!movie.id) return null;
 
     return (
-      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+      <MovieWrapper
+        data-testid="backdrop-path"
+        backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}
+      >
         <MovieInfo>
           <Overdrive id={`${movie.id}`}>
             <Poster
+              data-testid="poster-path"
               src={`${POSTER_PATH}${movie.poster_path}`}
               alt={movie.title}
             />
           </Overdrive>
           <div>
-            <h1>{movie.title}</h1>
-            <h3>{movie.release_date}</h3>
-            <p>{movie.overview}</p>
+            <h1 data-testid="movie-title">{movie.title}</h1>
+            <h3 data-testid="release-date">{movie.release_date}</h3>
+            <p data-testid="movie-overview">{movie.overview}</p>
           </div>
         </MovieInfo>
       </MovieWrapper>
--- src/MovieDetail.test.js
+++ src/MovieDetail.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
-import MovieDetail from './MovieDetail';
+import { render, cleanup, waitForElement } from 'react-testing-library';
+import MovieDetail, { BACKDROP_PATH, POSTER_PATH } from './MovieDetail';
 
 // sets fetch globally for entire test suite
 global.fetch = require('jest-fetch-mock');
@@ -20,16 +20,27 @@ const match = {
 
 console.error = jest.fn();
 
-test('<MovieDetail />', () => {
-  fetch.mockResponseOnce(
-    JSON.stringify({
-      movie: {
-        id: 'hello',
-        title: 'Level Up Is Great',
-      },
-    }),
-  );
+const movie = {
+  id: 'hello',
+  title: 'Level Up Is Great',
+  poster_path: 'noeifang',
+  release_date: 'March 11, 1989',
+  overview:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, non labore dolorem blanditiis soluta praesentium voluptas dolore numquam pariatur magnam at, deserunt neque sequi aperiam eius consequuntur? Aliquid, quaerat vitae.',
+};
+test('<MovieDetail />', async () => {
+  fetch.mockResponseOnce(JSON.stringify(movie));
 
-  const { debug } = render(<MovieDetail match={match} />);
-  debug();
+  const { getByTestId } = render(<MovieDetail match={match} />);
+  await waitForElement(() => getByTestId('movie-title'));
+  await waitForElement(() => getByTestId('poster-path'));
+  await waitForElement(() => getByTestId('release-date'));
+  await waitForElement(() => getByTestId('movie-overview'));
+
+  expect(getByTestId('movie-title').textContent).toBe(movie.title);
+  expect(getByTestId('release-date').textContent).toBe(movie.release_date);
+  expect(getByTestId('movie-overview').textContent).toBe(movie.overview);
+  expect(getByTestId('poster-path').src).toBe(
+    `${POSTER_PATH}${movie.poster_path}`,
+  );
 });
